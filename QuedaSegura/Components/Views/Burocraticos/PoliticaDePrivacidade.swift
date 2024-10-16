9fa18fb93c1cdf97c43743e385397206275d1492
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-10-16 14:16:09 -0300

    new commit

---
+++ QuedaSegura/Components/Views/Burocraticos/PoliticaDePrivacidade.swift
@@ -0,0 +1,18 @@
+//
+//  PoliticaDePrivacidade.swift
+//  QuedaSegura
+//
+//  Created by Mateus Felipe da Silveira Vieira on 15/10/24.
+//
+
+import SwiftUI
+
+struct PoliticaDePrivacidade: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoliticaDePrivacidade()
+}
